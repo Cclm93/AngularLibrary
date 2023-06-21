86e055e2a271d45bf54515a811c59d74613b8c07
Caio Monte <ccldm200@gmail.com>
2023-06-21 10:10:31 -0300

    Finalização

--- consumidor/src/app/app.module.ts
+++ consumidor/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Lib1Module } from 'lib1';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Lib1Module
   ],
   providers: [],
   bootstrap: [AppComponent]
